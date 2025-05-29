03f35724dfeb408423ef4abb4e85ac431d1c5106
swagatikasahu123 <155278112+swagatikasahu123@users.noreply.github.com>
2025-05-29 14:38:56 +0530

    Update QuizPage.jsx

--- client/src/pages/QuizPage.jsx
+++ client/src/pages/QuizPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import io from "socket.io-client";
-const socket = io("http://localhost:5000");
+const socket = io("https://vibe-check-mca6.onrender.com");
 
 const questions = [
   {
